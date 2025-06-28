1b102112cd8cea8770136e8fbc50f81aa967a010
Kaleb <Kalebreid15@gmail.com>
2025-06-28 19:07:11 -0400

    exerise 2 with bonus

---
+++ ORM-Dapper/DapperProductRepository.cs
@@ -0,0 +1,48 @@
+﻿using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ORM_Dapper;
+
+public class DapperProductRepository(IDbConnection conn) : IProductRepository
+{
+    private readonly IDbConnection _conn = conn;
+
+    public IEnumerable<Product> GetAllProducts()
+    {
+        return _conn.Query<Product>("SELECT * FROM Products;");
+    }
+
+    public Product GetProductById(int id)
+    {
+        return _conn.QuerySingle<Product>("SELECT * FROM Products WHERE ProductID = @Id;",
+            new { Id = id }
+        );
+    }
+
+    public void UpdateProduct(Product product)
+    {
+        _conn.Execute("UPDATE products" +
+            " SET name = @name," +
+            " Price = @price," + 
+            " CategoryID = @catID," + 
+            " OnSale = @onSale," + 
+            " StockLevel = @stock" +
+            " WHERE ProductID = @id",
+           new
+           {
+               id = product.ProductID,
+               name = product.Name,
+               price = product.Price,
+               catID = product.CategoryID,
+               onSale = product.OnSale,
+               stock = product.StockLevel
+           });
+    }
+}
+
--- ORM-Dapper/Program.cs
+++ ORM-Dapper/Program.cs
@@ -8,10 +8,16 @@ var config = new ConfigurationBuilder()
                 .AddJsonFile("appsettings.json")
                 .Build();
 
-string connString = config.GetConnectionString("DefaultConnection");
+string? connString = config.GetConnectionString("DefaultConnection");
+
+if (string.IsNullOrEmpty(connString))
+{
+    throw new InvalidOperationException("The connection string 'DefaultConnection' is not configured or is null.");
+}
 
 IDbConnection conn = new MySqlConnection(connString);
 
+#region
 var departmentRepository = new DapperDepartmentRepository(conn);
 
 departmentRepository.InsertDepartment("HR");
@@ -22,5 +28,32 @@ foreach (var department in departments)
 {
     Console.WriteLine(department.DepartmentID);
     Console.WriteLine(department.Name);
+    Console.WriteLine();
+}
+#endregion
+
+var productRepository = new DapperProductRepository(conn);
+
+var productToUpdate = productRepository.GetProductById(943);
+
+productToUpdate.Name = "Updated Product Name";
+productToUpdate.Price = 19.99;
+productToUpdate.CategoryID = 2;
+productToUpdate.OnSale = true;
+productToUpdate.StockLevel = 50;
+
+productRepository.UpdateProduct(productToUpdate);
+
+var products = productRepository.GetAllProducts();
+
+foreach (var product in products)
+{
+    Console.WriteLine(product.ProductID);
+    Console.WriteLine(product.Name);
+    Console.WriteLine(product.Price);
+    Console.WriteLine(product.CategoryID);
+    Console.WriteLine(product.OnSale);
+    Console.WriteLine(product.StockLevel);
+    Console.WriteLine();
     Console.WriteLine();
 }
